89bde2e0fdf1913ba068c94375479f06816a76d8
Lucas Feliciano <lucas.feliciano@bry.com.br>
2019-10-11 15:05:54 -0300

    Initial Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, AsyncStorage, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+
+export default function Home({ navigation }) {
+
+}
+
+const styles = StyleSheet.create({
+    
+})
